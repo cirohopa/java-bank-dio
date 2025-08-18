5c034f2104dd5069af3c57e061661a96f9b86abf
Ciro <cirohopa@gmail.com>
2025-08-18 09:35:56 -0300

    Model package is finnaly complete

--- src/main/java/br/com/dio/model/AccountWallet.java
+++ src/main/java/br/com/dio/model/AccountWallet.java
@@ -1,7 +1,24 @@
 package br.com.dio.model;
 
+import java.util.List;
+
 public class AccountWallet extends Wallet{
-    public AccountWallet(BankService serviceType) {
-        super(serviceType);
+
+    private final List<String> pix;
+
+    public AccountWallet(final List<String> pix) {
+        super(BankService.ACCOUNT);
+        this.pix = pix;
+    }
+
+    public AccountWallet(final long amount, List<String> pix) {
+        super(BankService.ACCOUNT);
+        this.pix = pix;
+        addMoney(amount, "Valor de criação da conta");
+    }
+
+    public void addMoney(final long amount, final String description){
+        var money = generateMoney(amount, description);
+        this.money.addAll(money);
     }
 }
